4f26cf81b47aa550c8ee84acff325c7f5897a2ab
José Delvalle <jd89041@gmail.com>
2019-02-26 23:24:14 -0300

    agregada lista de emails validos para el mensajero service

--- soporte/grails-app/services/soporte/MensajeroService.groovy
+++ soporte/grails-app/services/soporte/MensajeroService.groovy
@@ -5,13 +5,19 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class MensajeroService {
 
+    def emailsValidos = ["jd89041@gmail.com", "fitofitz@hotmail.com"]
     def mailService
 
     def enviarMail(destino, titulo, mensaje) {
-        mailService.sendMail {
-            to destino
-            subject titulo
-            text mensaje
+        if (emailsValidos.contains(destino)) {
+            mailService.sendMail {
+                to destino
+                subject titulo
+                text mensaje
+            }
         }
+        else
+            log.warn "No se va a enviar mail a ${destino}, no está disponible en la lista de emails válidos"
+
     }
 }
